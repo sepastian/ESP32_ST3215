939bc6fcb4d466ae09c46cf23eb129c06d0f8916
Sebastian Gassner <sebastian.gassner@gmail.com>
2024-10-03 16:30:43 +0200

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,27 @@
 /*
 Control an ST3215 magnetic encoder servo via UART from an ESP32,
-without using a dedicated controler board, for example [1].
-
-Based on "Secondary Development Tutorial" at [2].
-
-The ESP32 used is an "ESP32 DOIT DevKit V1",
-the ST3215 is from Waveshare [3].
-
-The ST3215 Servo uses a half-duplex serial communication,
-baud rate 1000000. Make sure to set `monitor_speed` in
-`platform.ini` to that speed when debugging over serial.
-
-Wiring:
-
-          .------------.
-          | .--------. |                  .--------.
-          | | ESP32  | |                  |        |=||
-          | `--------' |                  | ST3215 |
-          |            |                  |  Servo |
-   5V ----| VIN  17 TX |-- 10K -----------| RX     |
-  GND ----| GND  16 RX |--------'         |    PWR |---- 12V
-          `------------'                  |    GND |---- GND
-                                          `--------'
-
-[1] https://www.waveshare.com/wiki/Servo_Driver_with_ESP32.
-[2] https://www.waveshare.com/wiki/ST3215_Servo
-[3] https://www.waveshare.com/st3215-servo.htm
+without using a dedicated controler board.
 */
 
 #include <Arduino.h>
 #include <SCServo.h>
 
-
+// Pins for DOIT DevKit V1, adapt as required.
 #define RX 16
 #define TX 17
-#define LED 2
-#define SERVO_ID 1 // default for new servos
+#define LED 2      // built-in LED
+#define SERVO_ID 1 // default ID for new servos
 
 SMS_STS st;
 
+/*
+Blink the ESP32 built-in LED for a certain number of times.
+*/
 void blink(int times, int on_ms, int off_ms)
 {
   digitalWrite(LED, LOW);
   for (int i = 0; i < times; ++i)
   {
-    Serial.println(i);
-    Serial.println(times);
-    Serial.println(on_ms);
-    Serial.println(off_ms);
     delay(off_ms);
     digitalWrite(LED, HIGH);
     delay(on_ms);
@@ -73,12 +47,12 @@ void loop()
 {
   blink(1, 100, 100);
 
-  st.WritePosEx(1, 0, 500, 100); // Control the servo with ID 1 to rotate to the position of 1000 at a speed of 1500 and start and stop the acceleration of 50.
+  st.WritePosEx(SERVO_ID, 0, 500, 100); // Rotate to position 0 at speed 500, acceleration 50 at start/stop.
   for (auto i = 0; i < 360; ++i)
   {
-    st.WritePosEx(1, 4096 / 360 * i, 500, 100); // Control the servo with ID 1 to rotate to the position 20 at a speed of 1500 and start and stop the acceleration at 50.
+    st.WritePosEx(SERVO_ID, 4096 / 360 * i, 500, 100); // Step forward.
     delay(100);
   }
-  delay(1000); //[(P1-P0)/V]*1000+100
+  delay(1000);
   blink(2, 100, 100);
 }
